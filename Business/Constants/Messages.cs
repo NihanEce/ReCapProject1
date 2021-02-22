760337428bdc355a3a343ad81358eeca32906571
Nihan Ece Algül <nihanecealgul@gmail.com>
2021-02-22 17:17:52 +0300

    10 Şubat Ödev 4

--- Business/Constants/Messages.cs
+++ Business/Constants/Messages.cs
@@ -8,10 +8,36 @@ namespace Business.Constants
     public static class Messages
     {
         public static string CarAdded = "Araba eklendi";
-        public static string DailyPriceInvalid = "Girdiğiniz fiyat geçersiz. Lütfen günlük fiyat kısmına 0'dan büyük bir değer giriniz. ";
         public static string CarDeleted = "Araba silindi";
         public static string CarUpdated = "Araba güncellendi";
-        internal static string MaintenanceTime ="Sistem bakımda";
-        internal static string CarsListed = "Arabalar listelendi";
+        public static string CarsListed = "Arabalar listelendi";
+        public static string FailedCarAddOrUpdate = "Lütfen günlük fiyat kısmını 0'dan büyük giriniz.";
+
+        public static string BrandAdded = "Marka eklendi";
+        public static string BrandDeleted = "Marka silindi";
+        public static string BrandUpdated = "Marka güncellendi";
+        public static string FailedBrandAddOrUpdate = "Lütfen marka isminin uzunluğunu 2 karakterden fazla giriniz.";
+
+        public static string CustomerAdded = "Müşteri eklendi";
+        public static string CustomerDeleted = "Müşteri silindi";
+        public static string CustomerUpdated = "Müşteri güncellendi";
+
+        public static string UserAdded = "Kullanıcı eklendi";
+        public static string UserDeleted = "Kullanıcı silindi";
+        public static string UserUpdated = "Kullanıcı güncellendi";
+
+        public static string RentalAdded = "Araba kiralama işlemi başarıyla gerçekleşti";
+        public static string RentalDeleted = "Araba kiralama işlemi iptal edildi";
+        public static string RentalUpdated = "Araba kiralama işlemi güncellendi";
+        public static string FailedRentalAddOrUpdate = "Bu araba henüz teslim edilmediği için kiralama işlemi yapamazsınız";
+        public static string RentalReturned = "Kiraladığınız araç teslim edildi.";
+
+
+
+        public static string DailyPriceInvalid = "Girdiğiniz fiyat geçersiz. Lütfen günlük fiyat kısmına 0'dan büyük bir değer giriniz. ";
+        public static string MaintenanceTime = "Sistem bakımda";
+
+
     }
+
 }
