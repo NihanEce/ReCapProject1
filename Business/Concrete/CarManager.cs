68ecccaf9aa2f3bd7c995d8bff615a9644e6fdeb
Nihan Ece Algül <nihanecealgul@gmail.com>
2021-02-27 20:21:26 +0300

    12.Gün ÖDev 2 Fluent Validation

--- Business/Concrete/CarManager.cs
+++ Business/Concrete/CarManager.cs
@@ -1,9 +1,12 @@
 ﻿using Business.Abstract;
 using Business.Constants;
+using Business.ValidationRules.FluentValidation;
+using Core.CrossCuttingConcerns.Validation;
 using Core.Utilities.Results;
 using DataAccess.Abstract;
 using Entities.Concrete;
 using Entities.DTOs;
+using FluentValidation;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -22,16 +25,11 @@ namespace Business.Concrete
 
         public IResult Add(Car car)
         {
-            if (car.DailyPrice > 0)
-            {
-                _carDal.Add(car);
-                return new SuccessResult(Messages.CarAdded);
+            ValidationTool.Validate(new CarValidator(), car);
 
-            }
-            else
-            {
-                return new ErrorResult(Messages.DailyPriceInvalid);
-            }
+
+            _carDal.Add(car);
+            return new SuccessResult(Messages.CarAdded);
 
         }
 
